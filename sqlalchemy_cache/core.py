0502b587bd475fca6415cdc4cd02005dae702c10
lyncir <lyncir@gmail.com>
2016-12-07 17:42:29 +0800

    add lock.TODO: database:table:id -> hmset

--- sqlalchemy_cache/core.py
+++ sqlalchemy_cache/core.py
@@ -2,6 +2,7 @@
 
 import pickle
 from hashlib import md5
+from contextlib import contextmanager
 from redis import StrictRedis
 from sqlalchemy.orm.interfaces import MapperOption
 from sqlalchemy.orm.query import Query
@@ -233,3 +234,72 @@ class Cache(object):
 
     def delete(self, key):
         return self.cache.delete(key)
+
+    def blpop(self, *args, **kwargs):
+        return self.cache.blpop(*args, **kwargs)
+
+    def rpush(self, *args, **kwargs):
+        return self.cache.rpush(*args, **kwargs)
+
+    def lpush(self, *args, **kwargs):
+        return self.cache.lpush(*args, **kwargs)
+
+    def getset(self, *args, **kwargs):
+        return self.cache.getset(*args, **kwargs)
+
+
+class Lock(object):
+    """Lock implemented on top of redis."""
+
+    def __init__(self, client, name, timeout=60, db=0):
+        """
+        Create, if necessary the lock variable in redis.
+        We utilize the ``blpop`` command and its blocking behavior.
+        The ``_key`` variable is used to check, whether the mutex exists or not,
+        while the ``_mutex`` variable is the actual mutex.
+        """
+        self._key = 'lock:name:%s' % name
+        self._mutex = 'lock:mutex:%s' % name
+        self._timeout = timeout
+        self._r = client
+        self._init_mutex()
+
+    @property
+    def mutex_key(self):
+        return self._mutex
+
+    def lock(self):
+        """
+        Lock and block.
+
+         Raises:
+             RuntimeError, in case of synchronization issues.
+        """
+        res = self._r.blpop(self._mutex, self._timeout)
+        if res is None:
+            raise RuntimeError
+
+    def unlock(self):
+        self._r.rpush(self._mutex, 1)
+
+    def _init_mutex(self):
+        """
+        Initialize the mutex, if necessary.
+
+        Use a separate key to check for the existence of the "mutex",
+        so that we can utilize ``getset``, which is atomic.
+        """
+        exists = self._r.getset(self._key, 1)
+        if exists is None:
+            self._r.lpush(self._mutex, 1)
+
+
+@contextmanager
+def lock(client, name, timeout=60):
+    """Lock on name using redis."""
+    l = Lock(client, name, timeout)
+    try:
+        l.lock()
+        yield
+    finally:
+        l.unlock()
